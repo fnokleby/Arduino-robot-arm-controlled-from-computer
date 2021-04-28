084887ffa240340248e40d35fa56dd274eb0fadc
Filip Nøkleby <fnokleby@gmail.com>
2021-04-28 23:07:39 +0200

    Added controls

--- controller.py
+++ controller.py
@@ -5,20 +5,51 @@ import time
 
 arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
 
-def write_slogan():
-    print("Tkinter is easy to use!")
+def test_arm():
+    print("Sending command...")
+    arduino.write(bytes('1', 'utf-8'))
+    print('Command sent!')
+
+def reset_arm():
+    print("Sending command...")
+    arduino.write(bytes('2:55', 'utf-8'))
+    print('Command sent!')
+
+def send_arm_to_pos():
+    base = str(baseSlider.get())
+    arm1 = str(arm1Slider.get())
+    fullCommand = '3:' + base + '-' + arm1
+    print(fullCommand)
+    arduino.write(bytes(fullCommand, 'utf-8'))
 
 root = tk.Tk()
+root.geometry('500x500')
 frame = tk.Frame(root)
 frame.pack()
 
 # set window title
 root.wm_title("Robot controll")
 
-slogan = tk.Button(frame,
-                   text="Hello",
-                   command=write_slogan)
-slogan.pack(side=tk.LEFT)
+testButton = tk.Button(frame, text="Test arm", command=test_arm)
+testButton.grid(column=0, row = 1)
+
+returnButton = tk.Button(frame, text="Return arm to 0", command=reset_arm)
+returnButton.grid(column=1, row = 1)
+
+posButton = tk.Button(frame, text="Send arm to cords", command=send_arm_to_pos)
+posButton.grid(column=2, row = 1)
+
+baseSlider = tk.Scale(frame, from_=0, to=180, orient="horizontal", label="Base servo", )
+baseSlider.grid(column=0, row = 2)
+
+arm1Slider = tk.Scale(frame, from_=0, to=85, orient="horizontal", label="Arm 1", )
+arm1Slider.grid(column=1, row = 2)
+
+arm2Slider = tk.Scale(frame, from_=0, to=85, orient="horizontal", label="Arm 2", )
+arm2Slider.grid(column=2, row = 2)
+
+speedSlider = tk.Scale(frame, from_=0, to=85, orient="horizontal", label="Speed", )
+speedSlider.grid(column=3, row = 2)
 
 # show window
 root.mainloop()
