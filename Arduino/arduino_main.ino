6fa76811537d240261e5d9e35cd407b7098f1cf0
Filip Nøkleby <fnokleby@gmail.com>
2021-06-11 22:07:20 +0200

    Added temporary ps2 keyboard support

--- Arduino/arduino_main.ino
+++ Arduino/arduino_main.ino
@@ -3,8 +3,8 @@
 #include <LiquidCrystal.h>
 #include <PS2Keyboard.h>
 
-const int DataPin = A3;
-const int IRQpin = A5;
+const int DataPin = 13;
+const int IRQpin = 2;
 
 PS2Keyboard keyboard;
 
@@ -84,18 +84,25 @@ void screenPos()
 {
   if (keyboard.available())
   {
-    char c = keyboard.read();
-  }
-  int screenBase = servo1.read() + 1;
-  int screenArm1 = servo2.read() + 1;
-  lcd.setCursor(0, 0);
-  lcd.print("Starting!");
-  lcd.setCursor(0, 1);
-  lcd.print("Base:");
-  lcd.print(screenBase);
 
-  lcd.print(" Arm1:");
-  lcd.print(screenArm1);
+    char c = keyboard.read();
+
+    lcd.setCursor(0, 0);
+    lcd.print("Starting!");
+  }
+  else
+  {
+    int screenBase = servo1.read() + 1;
+    int screenArm1 = servo2.read() + 1;
+    lcd.setCursor(0, 0);
+    lcd.print("Starting!");
+    lcd.setCursor(0, 1);
+    lcd.print("Base:");
+    lcd.print(screenBase);
+
+    lcd.print(" Arm1:");
+    lcd.print(screenArm1);
+  }
 }
 
 void moveArm1(int x, int v)
@@ -120,127 +127,181 @@ void moveArm1(int x, int v)
 
 void loop()
 {
-  screenPos();
-  if (Serial.available() == 0)
+  if (keyboard.available())
   {
 
-    return;
+    // read the next key
+    char c = keyboard.read();
+
+    // check for some of the special keys
+    if (c == PS2_ENTER)
+    {
+      lcd.setCursor(0,1);
+    }
+    else if (c == PS2_TAB)
+    {
+      lcd.print("[Tab]");
+    }
+    else if (c == PS2_ESC)
+    {
+      lcd.print("[ESC]");
+    }
+    else if (c == PS2_PAGEDOWN)
+    {
+      lcd.print("[PgDn]");
+    }
+    else if (c == PS2_PAGEUP)
+    {
+      lcd.print("[PgUp]");
+    }
+    else if (c == PS2_LEFTARROW)
+    {
+      lcd.print("[Left]");
+    }
+    else if (c == PS2_RIGHTARROW)
+    {
+      lcd.print("[Right]");
+    }
+    else if (c == PS2_UPARROW)
+    {
+      lcd.print("[Up]");
+    }
+    else if (c == PS2_DOWNARROW)
+    {
+      lcd.print("[Down]");
+    }
+    else if (c == PS2_DELETE)
+    {
+      lcd.print("[Del]");
+    }
+    else
+    {
+
+      // otherwise, just print all normal characters
+      lcd.print(c);
+    }
   }
 
-  doc.clear();
-  String serialContent = Serial.readString();
-  Serial.print(serialContent);
-  deserializeJson(doc, serialContent);
-  Serial.print(serialContent);
-
-  int commandNum = doc["action"];
-  String basePos = doc["baseDegrees"];
-  String arm1Pos = doc["arm1Degrees"];
-  String delayStr = doc["speed"];
-  String saveWaitTime = doc["waitTime"];
-  int waitTime = saveWaitTime.toInt();
-
-  int basePosInt = basePos.toInt();
-  int arm1PosInt = arm1Pos.toInt();
-  int delayInt = delayStr.toInt();
-
-  switch (commandNum)
-  {
-  case 1:
-    for (i = 0; i < 180; i++)
+    if (Serial.available() == 0)
     {
-      servo1.write(i);
-      delay(10);
+
+      return;
     }
 
-    for (i = 0; i < 80; i++)
-    {
-      servo2.write(i);
-      delay(10);
-    }
-    for (i = 80; i > 0; i--)
-    {
-      servo2.write(i);
-      delay(10);
-    }
+    doc.clear();
+    String serialContent = Serial.readString();
+    Serial.print(serialContent);
+    deserializeJson(doc, serialContent);
+    Serial.print(serialContent);
 
-    for (i = 180; i > 0; i--)
-    {
-      servo1.write(i);
-      delay(10);
-    }
-    break;
-  case 2:
-    moveBase(0, delayInt);
-    moveArm1(0, delayInt);
-    break;
-  case 3:
-    moveBase(basePosInt, delayInt);
-    moveArm1(arm1PosInt, delayInt);
-    break;
-  case 11:
-    base1 = servo1.read();
-    arm11 = servo2.read();
-    waitTime1 = waitTime;
-    break;
-  case 12:
-    base2 = servo1.read();
-    arm12 = servo2.read();
-    waitTime2 = waitTime;
-    break;
-  case 13:
-    base3 = servo1.read();
-    arm13 = servo2.read();
-    waitTime3 = waitTime;
-    break;
-  case 14:
-    base4 = servo1.read();
-    arm14 = servo2.read();
-    waitTime4 = waitTime;
-    break;
-  case 21:
-    moveBase(base1, delayInt);
-    moveArm1(arm11, delayInt);
-    break;
-  case 22:
-    moveBase(base2, delayInt);
-    moveArm1(arm12, delayInt);
-    break;
-  case 23:
-    moveBase(base3, delayInt);
-    moveArm1(arm13, delayInt);
-    break;
-  case 24:
-    moveBase(base4, delayInt);
-    moveArm1(arm14, delayInt);
-    break;
-  case 29:
-    moveBase(base1, delayInt);
-    moveArm1(arm11, delayInt);
-    delay(waitTime1);
-    moveBase(base2, delayInt);
-    moveArm1(arm12, delayInt);
-    delay(waitTime2);
-    moveBase(base3, delayInt);
-    moveArm1(arm13, delayInt);
-    delay(waitTime3);
-    moveBase(base4, delayInt);
-    moveArm1(arm14, delayInt);
-    delay(waitTime4);
-    break;
-  case 35:
-    base1 = 0;
-    base2 = 0;
-    base3 = 0;
-    base4 = 0;
+    int commandNum = doc["action"];
+    String basePos = doc["baseDegrees"];
+    String arm1Pos = doc["arm1Degrees"];
+    String delayStr = doc["speed"];
+    String saveWaitTime = doc["waitTime"];
+    int waitTime = saveWaitTime.toInt();
 
-    arm11 = 0;
-    arm12 = 0;
-    arm13 = 0;
-    arm14 = 0;
-    break;
-  default:
-    // statements
-    break;
+    int basePosInt = basePos.toInt();
+    int arm1PosInt = arm1Pos.toInt();
+    int delayInt = delayStr.toInt();
+
+    switch (commandNum)
+    {
+    case 1:
+      for (i = 0; i < 180; i++)
+      {
+        servo1.write(i);
+        delay(10);
+      }
+
+      for (i = 0; i < 80; i++)
+      {
+        servo2.write(i);
+        delay(10);
+      }
+      for (i = 80; i > 0; i--)
+      {
+        servo2.write(i);
+        delay(10);
+      }
+
+      for (i = 180; i > 0; i--)
+      {
+        servo1.write(i);
+        delay(10);
+      }
+      break;
+    case 2:
+      moveBase(0, delayInt);
+      moveArm1(0, delayInt);
+      break;
+    case 3:
+      moveBase(basePosInt, delayInt);
+      moveArm1(arm1PosInt, delayInt);
+      break;
+    case 11:
+      base1 = servo1.read();
+      arm11 = servo2.read();
+      waitTime1 = waitTime;
+      break;
+    case 12:
+      base2 = servo1.read();
+      arm12 = servo2.read();
+      waitTime2 = waitTime;
+      break;
+    case 13:
+      base3 = servo1.read();
+      arm13 = servo2.read();
+      waitTime3 = waitTime;
+      break;
+    case 14:
+      base4 = servo1.read();
+      arm14 = servo2.read();
+      waitTime4 = waitTime;
+      break;
+    case 21:
+      moveBase(base1, delayInt);
+      moveArm1(arm11, delayInt);
+      break;
+    case 22:
+      moveBase(base2, delayInt);
+      moveArm1(arm12, delayInt);
+      break;
+    case 23:
+      moveBase(base3, delayInt);
+      moveArm1(arm13, delayInt);
+      break;
+    case 24:
+      moveBase(base4, delayInt);
+      moveArm1(arm14, delayInt);
+      break;
+    case 29:
+      moveBase(base1, delayInt);
+      moveArm1(arm11, delayInt);
+      delay(waitTime1);
+      moveBase(base2, delayInt);
+      moveArm1(arm12, delayInt);
+      delay(waitTime2);
+      moveBase(base3, delayInt);
+      moveArm1(arm13, delayInt);
+      delay(waitTime3);
+      moveBase(base4, delayInt);
+      moveArm1(arm14, delayInt);
+      delay(waitTime4);
+      break;
+    case 35:
+      base1 = 0;
+      base2 = 0;
+      base3 = 0;
+      base4 = 0;
+
+      arm11 = 0;
+      arm12 = 0;
+      arm13 = 0;
+      arm14 = 0;
+      break;
+    default:
+      // statements
+      break;
+    }
   }
-}
